b35c006197d0f68dd1cbf415e0c2f7de492346c2
Aske Merci <rainman1011@tuta.io>
2021-05-16 19:50:16 +0200

    Optimized fibonacci, at 100000 decimals, it took 89ms, got it down to 7ms

--- algorithms/Fibonacci/main.py
+++ algorithms/Fibonacci/main.py
@@ -1,25 +1,33 @@
 from config import limits
-
+from gmpy2 import mpz
 
 class Fibonacci:
     def __init__(self, n):
         self.n = int(n[0])
 
-    def iter(self):
-        a = 0
-        b = 1
-        if self.n < 0 or self.n > limits["fibonacci"]:
-            return str(0)
-        elif self.n == 0:
-            return str(a)
-        elif self.n == 1:
-            return str(b)
-        else:
-            for i in range(2, self.n + 1):
-                c = a + b
-                a = b
-                b = c
-            return str(b)
+    def fibonacci(self):
+        def fib_inner(n):
+            if n == 0:
+                return mpz(2), mpz(1)
+            m = n >> 1
+            u, v = fib_inner(m)
+            q = (2, -2)[m & 1]
+            u = u * u - q
+            v = v * v + q
+            if (n & 1):
+                return v - u, v
+            return u, v - u
+
+        m = self.n >> 1
+        u, v = fib_inner(m)
+
+        f = (2 * v - u) / 5
+        if (self.n & 1):
+            q = (self.n & 2) - 1
+            return v * f - q
+        return u * f
 
     def get(self):
-        return self.iter()
+        if self.n > limits['fibonacci']:
+            return str(0)
+        return str(int(self.fibonacci()))
